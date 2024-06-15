37ace1649e93b9551bf83213633de061ec6a7b08
Dev Patel <dpatel222430@gmail.com>
2024-06-15 16:41:50 +0530

    Initial Commit

---
+++ To_Do_app/To_Do_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_appApp.swift
+//  To_Do_app
+//
+//  Created by Dev Patel on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
